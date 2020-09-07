2833b9c61ab429b0d4699a5aed93a6fe5bc30e70
jatinbharadwaj <jitinrcks@gmail.com>
2020-09-07 18:25:30 +0530

    Comments added to controller

---
+++ src/controllers/comments.js
@@ -0,0 +1,58 @@
+const { Posts, Users, db, Comments } = require('../models/models')
+
+async function createNewComment(title,body,likesCount,userId,postId){
+    const comment = Comments.create({
+        title,
+        body,
+        likesCount,
+        userId,
+        postId
+    })
+    return comment
+}
+
+async function findAllComments(query){
+    let where = {}
+
+    if(query.userId && query.postId){
+        where.userId = query.userId
+        where.postId = query.postId
+    }
+
+    const comments = await Comments.findAll({
+        include:[Users,Posts],
+        where
+    })
+
+    return comments
+}
+
+module.exports = {
+    createNewComment,
+    findAllComments
+}
+
+// TEST 
+
+async function task(){
+    console.log(
+        await createNewComment(
+            'title1',
+            'body1',
+            4,
+            1,
+            1
+        )
+    )
+    console.log(
+        await createNewComment(
+            'title2',
+            'body2',
+            2,
+            1,
+            2
+        )
+    )
+}
+
+task()
